4c1255f7eacdc7a5b846a2065befcc147e9207cf
David Poslt <david.poslt@gmail.com>
2020-03-17 14:46:02 +0100

    add check full directory

--- checkFolder.py
+++ checkFolder.py
@@ -11,7 +11,7 @@ def checkFolder(folderName):
     else:
 
         os.mkdir(folderName)
-
+        return True
         
 
 
---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python env
+
+## head main.py ##
+
+
+__author__ = 'David Poslt'
+__version__ = '1.0.0'
+__date__ = '17.3.2020'
+
+
+import ini,os,log
+from checkFolder import checkFolder
+
+CONST_ZIP = ini.zipFile()
+CONST_TARGET = ini.target()
+CONST_DEPLOY = ini.deploy()
+
+def isFull(const_path):
+    if checkFolder(const_path):
+        count = len(os.listdir(const_path))
+        if count > 1:
+             print('uzip file')
+             log.info('unzip file')
+
+
+isFull(CONST_ZIP)
+
